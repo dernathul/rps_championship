bd9e2428abd0b4170627a33debdf13b2d287c5df
dernathul <lautaroparracab@gmail.com>
2020-03-04 19:51:01 +0100

    trying

--- cypress/integration/userCanChooseTheirMove.feature.js
+++ cypress/integration/userCanChooseTheirMove.feature.js
@@ -7,6 +7,15 @@ describe('User can choose a move', () => {
 	it('user choose a move', () => {
 		cy.visit('http://localhost:3001')
         cy.contains('Make your move')
-        cy.get('#Rock').click()
+        cy.contains('#Rock').click()
 	})
 })
+// describe("page can show results", () => {
+// 	beforeEach('round', () => {
+// 		cy.visit("http://localhost:3001")
+// 		cy.get('#r').click();
+// 	})
+// 	describe("the game is a tie", () => {
+// 		beforeEach("round")
+// 	})
+// })
